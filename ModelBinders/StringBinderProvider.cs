ad329db7642a883a8d9c0ccd5431e849a50aca46
SpiritBob <SpiritBob@users.noreply.github.com>
2020-07-08 10:43:43 +0300

    Update StringBinderProvider.cs

--- ModelBinders/StringBinderProvider.cs
+++ ModelBinders/StringBinderProvider.cs
@@ -8,6 +8,9 @@ namespace UnknownModelBindersExample.ModelBinders
 {
     public class StringBinderProvider : IModelBinderProvider
     {
+       // Gets hit multiple times - once where context.MetaData.ModelType is equal to `ExampleObject`, and then for each of its properties
+       // Why does this happen? Shouldn't it be hit only once with the complex type, and if no binder is provided - to provide one for each of its properties?
+       // What would happen if you provide a binder for the complex type and then for each of the properties - which one does its job first?
         public IModelBinder GetBinder(ModelBinderProviderContext context)
         {
             if (context == null)
